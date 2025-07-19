20c8d7fdf437eb239b4cac8a4f7bce8c9ce7fffd
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-07-19 23:06:50 +0530

    Start

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,27 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
+import type {Route} from "./+types/home";
+import {title} from "process";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    {title: "ProfilePolish"},
+    {
+      name: "description",
+      content: "Smart Feedback for your dream job resume..",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center min-h-screen">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Review your submissions and check AI-Powered feedback</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
