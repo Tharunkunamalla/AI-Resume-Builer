63dacb6212d63d5687fac6f4fdb67a83f41447ba
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-07-21 00:48:32 +0530

    bugs

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
-        <p className="text-2xl font-bold text-gradient">ProfilePolish</p>
+        <p className="text-2xl font-bold text-gradient">ResumeGenie AI</p>
       </Link>
 
       <Link to="/upload" className="primary-button w-fit">
